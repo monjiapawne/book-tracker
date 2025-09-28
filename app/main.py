c7ff8f0439424d6e72e046619f5fc20d14f9939c
monjiapawne <iambrendamore@gmail.com>
2025-09-28 00:22:17 -0400

    clean: main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,9 @@ from app.controller import main_logic
 from app.io import write_readme
 
 def main(export):
-    o = main_logic()
-    write_readme(o)
-    return o
+    output = main_logic()
+    write_readme(output)
 
 
 if __name__ == "__main__":
-    result = main(1)
+    main()
